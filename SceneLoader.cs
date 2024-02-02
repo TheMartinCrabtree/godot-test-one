32cb000e08cae585e3315a8beb3105d028a1825a
Martin Crabtree <martin.crabtree@gmail.com>
2024-02-02 17:32:17 -0500

    fixed accidental deletion

--- SceneLoader.cs
+++ SceneLoader.cs
@@ -1,11 +1,12 @@
 using Godot;
 using System;
 
+// need to test logic
 public partial class scene_loader : Node
 {
 	[Export] private string _sceneFolder;
 	public void ChangetoScene(string sceneName){
-		string f = _sceneFolder == "" ? : $"{_sceneFolder}/";
+		string f = _sceneFolder == "" ? "" : $"{_sceneFolder}/";
 		GetTree().ChangeSceneToFile($"res://{f}{sceneName}");
 	}
 }
