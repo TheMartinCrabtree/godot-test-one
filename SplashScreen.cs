7b8208a273f2a543a94c0dbb7a556b7dbd9cdb43
Martin Crabtree <martin.crabtree@gmail.com>
2024-02-06 11:29:07 -0500

    testing

--- SplashScreen.cs
+++ SplashScreen.cs
@@ -6,6 +6,8 @@ public partial class SplashScreen : ColorRect
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// get instance of timer class, add callback to timeout fn
+		// get scene_loader from auto loader, change to main menu
 		GetNode<Timer>("Timer").Timeout += 
 			() => GetNode<scene_loader>("/root/SceneLoader").ChangetoScene("main_menu.tscn");
 	}
